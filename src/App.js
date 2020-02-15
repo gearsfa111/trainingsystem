ee0e137eb5021e463ed0e0fc63fea9b42b29abce
gearnarachai <36767403+gearnarachai@users.noreply.github.com>
2020-02-15 14:10:20 +0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Signin from './view/authentication/signin/Signin';
 import Singup from './view/authentication/signup/Signup';
 import Sidebar from './view/layout/layout-component/Sidebar';
+import Navbar from './view/layout/layout-component/Navbar';
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <Sidebar />
+      <Navbar />
     );
   }
 }
