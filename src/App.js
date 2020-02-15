42884b433f0febee7f75bf4342c6506e8113b31c
gearnarachai <36767403+gearnarachai@users.noreply.github.com>
2020-02-15 11:39:27 +0700

    1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Signin from './view/authentication/signin/Signin';
-import Singup from './view/authentication/signup/Singup';
+import Singup from './view/authentication/signup/Signup';
 
 class App extends React.Component {
 
