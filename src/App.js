62727ef33a50ef093fce0b5aab1aef4a7e5eb223
gearnarachai <36767403+gearnarachai@users.noreply.github.com>
2020-02-15 14:02:30 +0700

    SideBar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './view/authentication/signin/Signin';
 import Singup from './view/authentication/signup/Signup';
+import Sidebar from './view/layout/layout-component/Sidebar';
+
 
 class App extends React.Component {
 
   render() {
     return (
-      <Singup />
+      <Sidebar />
     );
   }
 }
