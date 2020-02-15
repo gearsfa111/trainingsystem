730f5386a75f2892d83fd6181e43e96d73053037
gearnarachai <36767403+gearnarachai@users.noreply.github.com>
2020-02-15 11:06:57 +0700

    2

--- src/view/authentication/signin/Signin.js
+++ src/view/authentication/signin/Signin.js
@@ -1,40 +1,53 @@
 import React from 'react';
+import logo from '../../../assets/images/kru.png';
+import bg from '../../../assets/images/bg-01.jpg';
+
+import '../../../assets/style/customStyle.css';
+import '../../../assets/css/bootstrap.css';
+
+
+const bgImage = {
+    backgroundImage: "url(" + bg + ")",
+    backgroundRepeat: "no-repeat",
+
+};
 
 class Signin extends React.Component {
 
     render() {
         return (
-            <div class="container-login" style="background-image: url('assets/images/bg-01.jpg');">
-                <div class="col-md-12">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4">
-                        <div class="account-wall">
-                            <h4 class="text-center">เข้าสู่ระบบ</h4>
-                            <img class="logo-img" src="assets/images/kru.png" alt="" />
-                            <form class="form-signin" action="">
-                                <div class="txtInput">
-                                    <input type="text" class="form-control" placeholder="อีเมล" required autofocus />
+            <div className="container-login" style={bgImage}>
+                <div className="col-md-12">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <div className="account-wall">
+                            <h4 className="text-center">เข้าสู่ระบบ</h4>
+                            <img className="logo-img" src={logo} alt="" />
+                            <form className="form-signin" action="">
+                                <div className="txtInput">
+                                    <input type="text" className="form-control" placeholder="อีเมล" required autoFocus />
                                 </div>
-                                <div class="txtInput">
-                                    <input type="password" class="form-control" placeholder="รหัสผ่าน" required autofocus />
+                                <div className="txtInput">
+                                    <input type="password" className="form-control" placeholder="รหัสผ่าน" required autoFocus />
                                 </div>
-                                <div class="txtInput">
-                                    <button class="btn btn-sm btn-primary btn-block" type="submit">เข้าสู่ระบบ</button>
+                                <div className="txtInput">
+                                    <button className="btn btn-sm btn-primary btn-block" type="submit">เข้าสู่ระบบ</button>
                                 </div>
                                 <br />
-                                <label class=" pull-left">
+                                <label className=" pull-left">
                                     <input type="checkbox" value="remember-me" />
                                     จำฉันไว้
                                     </label>
-                                <a href="form_signup.html" class="pull-right">ยังไม่ได้เป็นสมาชิก? ลงทะเบียน </a>
+                                <a href="form_signup.html" className="pull-right">ยังไม่ได้เป็นสมาชิก? ลงทะเบียน </a>
 
                             </form>
                         </div>
                     </div>
-                    <div class="col-md-4"></div>
+                    <div className="col-md-4"></div>
                 </div>
             </div>
         );
     }
 }
+
 export default Signin;
