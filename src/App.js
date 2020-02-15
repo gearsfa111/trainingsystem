87d12e5e2547f087c77faa59fd78fd3cd59e3cd5
gearnarachai <36767403+gearnarachai@users.noreply.github.com>
2020-02-15 17:12:27 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Signin from './view/authentication/signin/Signin';
-import Singup from './view/authentication/signup/Signup';
-import Sidebar from './view/layout/layout-component/Sidebar';
-import Navbar from './view/layout/layout-component/Navbar';
-import FullLayout from './view/layout/FullLayout';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Blanklayout from './layouts/blanklayout';
+import FullLayout from './layouts/fulllayout';
+import Signin from './views/authentication/signin';
 
 class App extends React.Component {
-
   render() {
     return (
-      <FullLayout />
+      <HashRouter>
+        <Switch>
+          <Route path="/" name="authentication" component={Blanklayout} />
+        </Switch>
+      </HashRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+//import './index.css';
+import "./assets/style/customStyle.css";
+//import './assets/fontawesome-free/css/all.min.css'
 
+import "./assets/css/bootstrap.css";
+import "./assets/css/bootstrap-theme.css";
+//import './assets/css/sb-admin-2.css'
 
-import './assets/style/customStyle.css';
-import './assets/css/bootstrap.css';
-import './assets/css/bootstrap-theme.css';
-import './assets/vendor/fontawesome-free/css/all.min.css';
-import './assets/css/sb-admin-2.min.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
